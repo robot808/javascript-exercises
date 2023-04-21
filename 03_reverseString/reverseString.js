c7243402dfd69c52e7f91cf2610dbfda188c7355
robot808 <bob@cyber-wizard.com>
2023-04-21 15:46:36 -0500

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const array = string.split("");
+  const arrayReversed = array.reverse();
+  const stringReversed = arrayReversed.join("");
+  return stringReversed;
 };
 
 // Do not edit below this line

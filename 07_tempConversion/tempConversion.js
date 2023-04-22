935ff7de191651caac6945bc336ea86ab067890a
robot808 <bob@cyber-wizard.com>
2023-04-22 16:51:06 -0500

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  return Number(((degreesF - 32) / 1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  return Number((degreesC * 1.8 + 32).toFixed(1));
 };
 
 // Do not edit below this line

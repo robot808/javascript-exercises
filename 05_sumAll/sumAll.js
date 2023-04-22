907bf8e9548ccadabf14156095d466c1a50ac00a
robot808 <bob@cyber-wizard.com>
2023-04-22 04:00:31 -0500

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+  if (!Number.isInteger(number1) || !Number.isInteger(number2)) {
+    return "ERROR";
+  } 
+  else if (number1 < 0 || number2 < 0) {
+    return "ERROR";
+  }
 
+  let first, last;
+  if (number1 < number2) {
+    first = number1;
+    last = number2;
+  } 
+  else if (number1 > number2) {
+    first = number2;
+    last = number1;
+  } 
+  else {
+    return "ERROR";
+  }
+
+  let sum = 0;
+  for (let i = first; i <= last; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line

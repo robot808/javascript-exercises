4c41b78cc404c8bea3e3c20f04fd6429f852d993
robot808 <rsbradshaw88@protonmail.com>
2023-05-01 16:44:15 -0500

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, value) => total + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (!(arr.length)) return 0;
+  return arr.reduce((total, value) => total * value, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let product = a;
+	for (let i = 0; i < b - 1; i++) product *= a;
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let product = 1;
+  for (let i = 1; i <= a; i++) product *= i;
+  return product;
 };
 
 // Do not edit below this line

6cf1dd89840a372a35206871556cc2fb0ddfc7ab
robot808 <rsbradshaw88@protonmail.com>
2023-05-01 19:44:10 -0500

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+nth = parseInt(nth);
+if (nth < 0) return "OOPS";
+if (nth < 1) return 0;
+let number = 1;
+let previous = 0;
+  for (let i = 1; i < nth; i++) {
+    const current = number;
+    number = current + previous;
+    previous = current;
+  }
+return number;
 };
 
 // Do not edit below this line
